694afb8e85e323a37a7ad0b6830cd7a1f627dbda
Heli Sivunen <heli@mooncat.tv>
2020-12-14 09:37:57 +0000

    Gems and yarn updated, rails migrate.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :abouts
   get 'etusivus/index'
 
-  resources :etusivus
+  # resources :about, :contact
 
   root 'etusivus#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_101819) do
+ActiveRecord::Schema.define(version: 2020_08_28_143753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abouts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "etusivus", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
